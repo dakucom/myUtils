55fa42be0637f24b296f2b253d185a84e859e68b
qaz123456 <18566747679@163.com>
2021-02-07 10:35:36 +0800

    excel-hutool工具

---
+++ excel-hutool/src/main/java/com/dakuzai/hutool/ExcelHutoolApplication.java
@@ -0,0 +1,16 @@
+package com.dakuzai.hutool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: dakuzai
+ * @time: 2021/2/7 10:31
+ */
+@SpringBootApplication
+public class ExcelHutoolApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelHutoolApplication.class,args);
+    }
+}
